b5670436ba36ac0730efdb28e7bf0a88afb601d7
Maynor <Maynor@DESKTOP-S4O9LOU>
2021-03-29 19:24:54 -0600

    Sistematico

---
+++ src/main/java/ni/edu/uni/programacion/controllers/PnlViewsController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ni.edu.uni.programacion.controllers;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class PnlViewsController {
+
+    public PnlViewsController(Object pnlViews) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
